a768e1a846a8a0599053d33e8c3687d199265b04
Hamid Zade <hamidmamadzade@gmail.com>
2016-07-04 20:43:17 +0100

    can display most viewed

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,15 +1,20 @@
 class Parser
-  attr_reader :file_path, :popular_pages
+  attr_reader :file_path, :most_viewed
 
   def initialize(file_path)
     @file_path = file_path
-    @popular_pages = Hash.new(0)
+    @most_viewed = most_viewed_pages
   end
 
   def most_viewed_pages
     file = File.read(file_path).split(/\n+/)
     pages_views = file.map {|x| x.split(" ") }.each{ |x| x.pop }.flatten
-    pages_views.each { |v| @popular_pages[v] += 1 }
-    @popular_pages.sort_by {|_key, value| value}.reverse.to_h
+    popular_pages = Hash.new(0)
+    pages_views.each { |v| popular_pages[v] += 1 }
+    popular_pages.sort_by {|_key, value| value}.reverse.to_h
+  end
+
+  def display_most_viewed
+    @most_viewed.map{|k,v| "#{k} #{v} visits"}.join("\n")
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -11,10 +11,18 @@ describe Parser do
     '/contact' => 1,
     '/about' => 1
   }
+  results =
+    "/help_page/1 4 visits\n/home 2 visits\n/about/2 1 visits\n/index 1 visits\n/contact 1 visits\n/about 1 visits"
 
   describe '#most_viewed_pages' do
     it 'should open file path and return most viewed pages hash' do
       expect(parser.most_viewed_pages).to eq mostViewed
     end
-  end 
+  end
+
+  describe '#display_most_viewed' do
+    it 'should return results as string' do
+      expect(parser.display_most_viewed).to eq results
+    end
+  end
 end
