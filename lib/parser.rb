78c06f317dd6a3e2d19517661d2650952b031b79
Hamid Zade <hamidmamadzade@gmail.com>
2016-07-16 03:43:44 +0100

    plays with design +minor refactor

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,20 +1,13 @@
 class Parser
-  attr_reader :file_path, :most_viewed
 
   def initialize(file_path)
-    @file_path = file_path
-    @most_viewed = most_viewed_pages
+    @file_arr = date(file_path)
   end
 
   def most_viewed_pages
-    file = File.read(file_path).split(/\n+/)
-    pages_views = file.map {|x| x.split(" ") }.each{ |x| x.pop }.flatten
+    pages_views = @file_arr.map { |x| x.split(' ') }.each(&:pop).flatten
     popular_pages = Hash.new(0)
     pages_views.each { |v| popular_pages[v] += 1 }
-    popular_pages.sort_by {|_key, value| value}.reverse.to_h
-  end
-
-  def display_most_viewed
-    @most_viewed.map{|k,v| "#{k} #{v} visits"}.join("\n")
+    popular_pages.sort_by { |_key, value| value }.reverse.to_h
   end
 end
